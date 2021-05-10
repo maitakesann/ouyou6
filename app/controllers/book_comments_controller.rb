fc51b21571f722b148c9243fa9f730a566fd7f7b
maitakesann <kankore1204a@yahoo.co.jp>
2021-05-10 06:15:40 +0000

    コメントfinish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,10 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-    redirect_to books_path(book)
-    else
-    redirect_to books_path(book)
-    end
+    comment.book_id = @book.id
+    comment.save
+    
   end
 
   def destroy
